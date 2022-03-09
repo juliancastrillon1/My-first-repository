19a980537d0eaf4bf7f070f92705b2cae352ec4a
juliancastrillon1 <101157527+juliancastrillon1@users.noreply.github.com>
2022-03-09 13:23:15 -0800

    Add files via upload

---
+++ basics.R
@@ -0,0 +1,25 @@
+# load packages -------
+
+library(tidyverse)
+library(here)
+library(skimr)
+library(readxl)
+library(haven)
+library(datapasta)
+
+
+# read in data ------
+
+beaches <- read_csv(here("data","sydneybeaches.csv"))
+
+# exploring the data ------
+
+
+view(beaches)
+dim(beaches)
+str(beaches)
+glimpse(beaches)
+head(beaches)
+tail(beaches)
+summary(beaches)
+skim(beaches)
